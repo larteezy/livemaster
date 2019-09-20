d38376324056a8ebb0e7f1bcc4efc2d11ae452fd
Стажер Livemaster Практикум <stager1@livemaster.lan>
2019-09-20 09:58:37 +0300

    анимации осталось

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 $( function() 
 {
-    $('.cells__elem').click(function(){
+    $('.cells__elem').click(function(){ //При клике на невыделенный элемент 
         $( this ).toggleClass('selected');
         $( '#delete' ).removeAttr('disabled');
     });
 
-    $('.selected').click(function(){
+    $('.selected').click(function(){ //При клике на выделенный элемент
         $( this ).toggleClass('selected');
         /*if ($('.selected').length < 1) 
         {
@@ -13,56 +13,74 @@ $( function()
         }*/
     });
 
-    $('#delete').click(function(){
+    $('#delete').click(function(){ //При клике на кнопку Удалить
         //$( this ).attr('class', 'selected');
         var elements = $('.selected');
-            for (i = 0; i < elements.length; i++) {
-                elements[i].style.display = 'none';
-            }
-            $( '#refresh' ).removeAttr('disabled'); 
-            $( '#delete' ).attr('disabled', 'disabled'); 
+        for (i = 0; i < elements.length; i++) 
+            elements[i].style.display = 'none';
+
+        $( '#refresh' ).removeAttr('disabled'); 
+        $( '#delete' ).attr('disabled', 'disabled'); 
     });
 
-    $('#refresh').click(function(){
+    $('#refresh').click(function(){ //При клике на кнопку Сбросить
         var elements = $('.selected');
-            for (i = 0; i < elements.length; i++) {
-                elements[i].style.display = 'inline-block';
-                
-            }
-            elements.toggleClass('selected');
-            $( '#refresh' ).attr('disabled', 'disabled');    
+        for (i = 0; i < elements.length; i++) 
+            elements[i].style.display = 'inline-block';
+
+        elements.toggleClass('selected');
+        $( '#refresh' ).attr('disabled', 'disabled');    
     });
     
-    $('#b1').click(function(){
+    $('#b1').click(function(){  //При клике на кнопку увеличения размера
         var elements = $('.cells__elem');
-            for (i = 0; i < elements.length; i++) {
-                elements[i].style.width = '420px';
-                elements[i].style.height = '420px';
-                elements[i].style.lineHeight = '420px';
-                elements[i].style.fontSize = '200px';
-                //elements[i].style. = '420px';  
-            }  
+
+        for (i = 0; i < elements.length; i++) 
+        {
+            elements[i].style.width = '420px';
+            elements[i].style.height = '420px';
+            elements[i].style.lineHeight = '420px';
+            elements[i].style.fontSize = '200px'; 
+        }  
     });
 
-    $('#b2').click(function(){
+    $('#b2').click(function(){  //Кнопка уменьшения размера
         var elements = $('.cells__elem');
-            for (i = 0; i < elements.length; i++) {
-                elements[i].style.width = '200px';
-                elements[i].style.height = '200px';
-                elements[i].style.lineHeight = '200px';
-                elements[i].style.fontSize = '100px';
-                //elements[i].style. = '420px';  
-            }  
+
+        for (i = 0; i < elements.length; i++) 
+        {
+            elements[i].style.width = '200px';
+            elements[i].style.height = '200px';
+            elements[i].style.lineHeight = '200px';
+            elements[i].style.fontSize = '100px'; 
+        }  
     });
 
+    $('#sort__ascending').click(function(){  //Кнопка уменьшения размера
+        items = $('.cells .cells__elem');
+        arItems = $.makeArray(items);
 
-/*
-    $('.selected').click(function(){
-        $( this ).attr('class', 'cells__elem');
-    });*/
-    //$('#select option:first').prop('selected', true);
-    $( ".sortable" ).sortable();
-    //$( ".selectable" ).selectable();
+        arItems .sort(function(a, b) 
+        {
+            return $(a).data("sort") - $(b).data("sort")
+        });
+
+        $(arItems).appendTo(".cells");
+    });
+
+    $('#sort__descending').click(function(){  //Кнопка уменьшения размера
+        items = $('.cells .cells__elem');
+        arItems = $.makeArray(items);
+
+        arItems .sort(function(b, a) 
+        {
+            return $(a).data("sort") - $(b).data("sort")
+        });
+
+        $(arItems).appendTo(".cells");
+    });
+
+    $( ".sortable" ).sortable();    //Перетаскивание элементов
     //$( ".sortable" ).disableSelection();
 } );
 
