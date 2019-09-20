a2565903d17beff9ad7f11ec650261347b490f97
Стажер Livemaster Практикум <stager1@livemaster.lan>
2019-09-20 17:33:11 +0300

    Добавить

--- scripts/script.js
+++ scripts/script.js
@@ -33,6 +33,64 @@ $( function()
             elements[i].style.display = 'inline-block';*/
         $( '#refresh' ).attr('disabled', 'disabled');    
     });
+
+    $('#addcell').click(function(){ //При клике на кнопку Сбросить
+        items = $('.cells .cells__elem');
+        
+        arItems = $.makeArray(items);
+        arItems .sort(function(a, b) 
+        {
+            return $(a).data("sort") - $(b).data("sort")
+        });
+
+        for (i = 0; i < arItems.length; i++)
+        {
+            console.log(i);
+            
+            if ((i + 1) != arItems[i])
+            {
+                var ul = document.getElementById("sortable");
+                var li = document.createElement('li');
+                li.setAttribute('class', 'cells__elem');
+                li.setAttribute('datasort', i + 2);
+                li.appendChild(document.createTextNode(i + 2));
+            }
+        }
+        
+        //li.appendChild(document.createTextNode("9"));
+        //var a = document.createAttribute('value');
+        //a.value = '9';
+        //li.appendChild(a);
+        ul.appendChild(li);
+        //ul.appendChild(items[1]);
+        //$(".cells ul").append ('<li class="cells__elem" data-sort="9">9</li>');
+        console.log('<li class="cells__elem" data-sort="9">9</li>');
+        
+/*
+        arItems .sort(function(a, b) 
+        {
+            return $(a).data("sort") - $(b).data("sort")
+        });
+
+        /*
+/*
+        for (i = 0; i < arItems.length; i++)
+        {
+            if ((i + 1) != arItems[i])
+            {
+                arItems.push();
+            }
+        }*/
+
+        //$(".cells").items.
+        //$(".cells").push(arItems[0]);
+        //$(arItems).push(".cells");// .appendTo(".cells");
+        /*
+        var elements = $('.deleted');
+        for (i = 0; i < elements.length; i++) 
+            elements[i].style.display = 'inline-block';*/
+           
+    });
     
     $('#b1').click(function(){  //Кнопку увеличения размера
         $('.cells').toggleClass('bigcells', true); 
