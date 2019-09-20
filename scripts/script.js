18b26cb5b95acb63b1e27f14a6a45e681bfd7b68
Стажер Livemaster Практикум <stager1@livemaster.lan>
2019-09-20 14:45:59 +0300

    Готово

--- scripts/script.js
+++ scripts/script.js
@@ -2,61 +2,47 @@ $( function()
 {
     $('.cells__elem').click(function(){ //При клике на невыделенный элемент 
         $( this ).toggleClass('selected');
-        $( '#delete' ).removeAttr('disabled');
-    });
-
-    $('.selected').click(function(){ //При клике на выделенный элемент
-        $( this ).toggleClass('selected');
-        /*if ($('.selected').length < 1) 
+        if ($('.selected').length != 0)
+        {
+            $( '#delete' ).removeAttr('disabled');
+        }
+        else
         {
             $( '#delete' ).attr('disabled', 'disabled'); 
-        }*/
+        }
+
     });
 
     $('#delete').click(function(){ //При клике на кнопку Удалить
-        //$( this ).attr('class', 'selected');
+        /*
         var elements = $('.selected');
         for (i = 0; i < elements.length; i++) 
-            elements[i].style.display = 'none';
+            elements[i].style.display = 'none';*/
 
+        $('.selected').toggleClass('deleted', true);
+        $('.selected').toggleClass('selected', false);
         $( '#refresh' ).removeAttr('disabled'); 
         $( '#delete' ).attr('disabled', 'disabled'); 
     });
 
     $('#refresh').click(function(){ //При клике на кнопку Сбросить
-        var elements = $('.selected');
+        $('.deleted').toggleClass('deleted', false);
+        /*
+        var elements = $('.deleted');
         for (i = 0; i < elements.length; i++) 
-            elements[i].style.display = 'inline-block';
-
-        elements.toggleClass('selected');
+            elements[i].style.display = 'inline-block';*/
         $( '#refresh' ).attr('disabled', 'disabled');    
     });
     
-    $('#b1').click(function(){  //При клике на кнопку увеличения размера
-        var elements = $('.cells__elem');
-
-        for (i = 0; i < elements.length; i++) 
-        {
-            elements[i].style.width = '420px';
-            elements[i].style.height = '420px';
-            elements[i].style.lineHeight = '420px';
-            elements[i].style.fontSize = '200px'; 
-        }  
+    $('#b1').click(function(){  //Кнопку увеличения размера
+        $('.cells').toggleClass('bigcells', true); 
     });
 
     $('#b2').click(function(){  //Кнопка уменьшения размера
-        var elements = $('.cells__elem');
-
-        for (i = 0; i < elements.length; i++) 
-        {
-            elements[i].style.width = '200px';
-            elements[i].style.height = '200px';
-            elements[i].style.lineHeight = '200px';
-            elements[i].style.fontSize = '100px'; 
-        }  
+        $('.cells').toggleClass('bigcells', false);
     });
 
-    $('#sort__ascending').click(function(){  //Кнопка уменьшения размера
+    $('#sort__ascending').click(function(){  //Сортировать по возрастанию
         items = $('.cells .cells__elem');
         arItems = $.makeArray(items);
 
@@ -68,7 +54,7 @@ $( function()
         $(arItems).appendTo(".cells");
     });
 
-    $('#sort__descending').click(function(){  //Кнопка уменьшения размера
+    $('#sort__descending').click(function(){  //Сортировать по убыванию
         items = $('.cells .cells__elem');
         arItems = $.makeArray(items);
 
@@ -80,7 +66,11 @@ $( function()
         $(arItems).appendTo(".cells");
     });
 
-    $( ".sortable" ).sortable();    //Перетаскивание элементов
+    $( "#sortable" ).sortable({ //Перетаскивание элементов
+        placeholder: "placeforcell",
+        containment: ".container"
+    });    
+
     //$( ".sortable" ).disableSelection();
 } );
 
